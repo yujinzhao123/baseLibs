af953575ef35e009c558187aaa2206020b3a5ed7
yujinzhaohh <529790314@qq.com>
2018-06-07 09:19:48 +0800

    t

---
+++ yjzLib/baselib/src/main/java/com/app/yjzlibs/baselib/Test.java
@@ -0,0 +1,14 @@
+package com.app.yjzlibs.baselib;
+
+import android.util.Log;
+
+/**
+ * Created by yujinzhao on 18/6/7.
+ */
+
+public class Test {
+
+    public void t(){
+        Log.e("yjz","ttttt");
+    }
+}
