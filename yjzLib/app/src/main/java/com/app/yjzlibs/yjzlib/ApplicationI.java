23cc7c75ebcedde39dba5d37de0e17bb5a0283ec
yujinzhaohh <529790314@qq.com>
2018-06-07 11:18:26 +0800

    none 内存泄漏检测工具bug

--- yjzLib/app/src/main/java/com/app/yjzlibs/yjzlib/ApplicationI.java
+++ yjzLib/app/src/main/java/com/app/yjzlibs/yjzlib/ApplicationI.java
@@ -1,6 +1,10 @@
 package com.app.yjzlibs.yjzlib;
 
 import com.app.yjzlibs.baselib.BaseApplication;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
 
 /**
  * Created by yujinzhao on 18/6/7.
@@ -12,5 +16,13 @@ public class ApplicationI extends BaseApplication{
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+    private void initLogger() {
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder().showThreadInfo(false).tag("gnpad").build();
+        Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy) {
+            public boolean isLoggable(int priority, String tag) {
+                return false;
+            }
+        });
     }
 }
