e69e14add22d4e103d3b264b58f95308c85c6905
yujinzhaohh <529790314@qq.com>
2019-01-08 17:58:40 +0800

    bean

--- yjzLib/baselib/src/main/java/com/app/yjzlibs/baselib/tool/StringUtil.java
+++ yjzLib/baselib/src/main/java/com/app/yjzlibs/baselib/tool/StringUtil.java
@@ -17,4 +17,14 @@ public class StringUtil {
     public static String float2String(float f){
         return float2String(f,"0.00");
     }
+
+    /**
+     * float保留小数 四舍五入
+     * @param f
+     * @param N 几位小数
+     * @return
+     */
+    public static float float2N(float f,int N){
+        return (float)(Math.round(f*10*N))/(10*N);
+    }
 }
